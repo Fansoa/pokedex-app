44a1de954fedf5a73b2b118043464523bb16c04b
fansoa <fverin.contactpro@gmail.com>
2024-09-26 15:55:54 +0200

    create PokemonDetailsScreen

---
+++ app/pokemon/[id].tsx
@@ -0,0 +1,12 @@
+import { ThemedText } from "@/components/ThemedText"
+import { SafeAreaView } from "react-native"
+
+const PokemonDetailsScreen = () => {
+    return (
+        <SafeAreaView>
+            <ThemedText>PokemonDetailsScreen</ThemedText>
+        </SafeAreaView>
+    )
+}
+
+export default PokemonDetailsScreen;
