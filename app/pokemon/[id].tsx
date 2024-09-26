6fb1d4ef5c0365284e166cb93837b3c1710f60ce
fansoa <fverin.contactpro@gmail.com>
2024-09-26 16:39:00 +0200

    set color to background screen by pokemon type

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,22 +1,29 @@
 import { ThemedText } from "@/components/ThemedText"
-import { ActivityIndicator, SafeAreaView } from "react-native"
+import { ActivityIndicator, SafeAreaView, StyleSheet } from "react-native"
 import { useLocalSearchParams } from "expo-router"
 import { useFetchQuery } from "@/hooks/useFetchQuery"
 import { useThemeColor } from "@/hooks/useThemeColor"
+import { Colors } from "@/constants/Colors"
 
 const PokemonDetailsScreen = () => {
     const { id } = useLocalSearchParams()
     const themeColor = useThemeColor()
-
-    const { isFetching } = useFetchQuery(`/pokemon/${id}`)
+    const { data, isFetching } = useFetchQuery(`/pokemon/${id}`)
+    const basePokemonColor = Colors.type[data?.types?.[0]?.type?.name] || 'normal'
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={[styles.container, {backgroundColor: basePokemonColor }]}>
             {
                 isFetching ? <ActivityIndicator color={themeColor.tint}/> : <ThemedText>PokemonDetailsScreen</ThemedText>
             }
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    }
+})
 
 export default PokemonDetailsScreen;
