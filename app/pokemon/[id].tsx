f8bf90b66d83695e0b9d71c9eded817ed2f12d63
fansoa <fverin.contactpro@gmail.com>
2024-09-26 20:37:53 +0200

    add Header

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,9 +1,12 @@
 import { ThemedText } from "@/components/ThemedText"
-import { ActivityIndicator, SafeAreaView, StyleSheet } from "react-native"
+import { ActivityIndicator, Image, Pressable, StyleSheet, View } from "react-native"
+import { SafeAreaView } from "react-native-safe-area-context"
 import { useLocalSearchParams } from "expo-router"
 import { useFetchQuery } from "@/hooks/useFetchQuery"
 import { useThemeColor } from "@/hooks/useThemeColor"
 import { Colors } from "@/constants/Colors"
+import { router } from "expo-router"
+import Row from "@/components/Row"
 
 const PokemonDetailsScreen = () => {
     const { id } = useLocalSearchParams()
@@ -11,18 +14,45 @@ const PokemonDetailsScreen = () => {
     const { data, isFetching } = useFetchQuery(`/pokemon/${id}`)
     const basePokemonColor = Colors.type[data?.types?.[0]?.type?.name] || 'normal'
 
+    const handleBackButton = () => {
+        router.back()
+    }
+
     return (
         <SafeAreaView style={[styles.container, {backgroundColor: basePokemonColor }]}>
-            {
-                isFetching ? <ActivityIndicator color={themeColor.tint}/> : <ThemedText>PokemonDetailsScreen</ThemedText>
-            }
+            <View>
+                {
+                    isFetching ? <ActivityIndicator color={themeColor.tint}/> : (
+                    <Row style={styles.header}>
+                        <Pressable onPress={handleBackButton}>
+                            <Image source={require('@/assets/images/arrow_back.png')}/>
+                        </Pressable>
+                        <ThemedText style={styles.pokemonName} type="headline">{data?.name}</ThemedText>
+                        <ThemedText type="subtitle2">#{id?.toString().padStart(3,'0')}</ThemedText>
+                    </Row>)
+                }
+                <Image style={styles.backgroundImg} source={require('@/assets/images/big_pokeball.png')}/>
+            </View>
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+    },
+    header: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 20,
+    },
+    pokemonName: {
+        textTransform: 'capitalize'
+    },
+    backgroundImg: {
+        position: 'absolute',
+        top: 8,
+        right: 8,
     }
 })
 
