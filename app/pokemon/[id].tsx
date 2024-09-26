22d4048dbd25c13fd4aece7f3d9c31e65bb8d6fd
fansoa <fverin.contactpro@gmail.com>
2024-09-26 16:09:14 +0200

    fetch pokemon depending on id params

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,10 +1,13 @@
 import { ThemedText } from "@/components/ThemedText"
 import { SafeAreaView } from "react-native"
 import { useLocalSearchParams } from "expo-router"
+import { useFetchQuery } from "@/hooks/useFetchQuery"
 
 const PokemonDetailsScreen = () => {
     const { id } = useLocalSearchParams()
-    
+
+    const { data } = useFetchQuery(`/pokemon/${id}`)
+
     return (
         <SafeAreaView>
             <ThemedText>PokemonDetailsScreen</ThemedText>
