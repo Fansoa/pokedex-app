ca4aa63e3709c030b94a6d32cf7d408765878f2c
fansoa <fverin.contactpro@gmail.com>
2024-09-30 21:51:31 +0200

    display some of pokemon info

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -7,15 +7,26 @@ import { useThemeColor } from "@/hooks/useThemeColor"
 import { Colors } from "@/constants/Colors"
 import { router } from "expo-router"
 import Row from "@/components/Row"
+import statShortName from "@/methods/statShortName"
+import StatsView from "@/components/StatsView"
 
 const PokemonDetailsScreen = () => {
     const { id } = useLocalSearchParams()
     const themeColor = useThemeColor()
     const { data, isFetching } = useFetchQuery(`/pokemon/${id}`)
+    const { data: species } = useFetchQuery(`/pokemon-species/${id}`)
     const basePokemonColor = Colors.type[data?.types?.[0]?.type?.name] || 'normal'
-
+    const abilityList = data?.abilities.map(({ability}) => ability.name)
+    const statList = data?.stats.reduce((acc, curr) => {
+        return [...acc, {name: statShortName(curr.stat.name), value: curr.base_stat}]
+    }, [])
+    const abilitiesDisplay = abilityList?.join(', ')
+    const bio = species?.flavor_text_entries
+    ?.find(({language}) => language.name === 'en')
+    ?.flavor_text.replaceAll('\n', ' ')
+    
     const handleBackButton = () => {
-        router.back()
+        router.navigate('')
     }
 
     return (
@@ -28,7 +39,7 @@ const PokemonDetailsScreen = () => {
                             <Pressable onPress={handleBackButton}>
                                 <Image source={require('@/assets/images/arrow_back.png')}/>
                             </Pressable>
-                            <ThemedText style={styles.pokemonName} type="headline">{data?.name}</ThemedText>
+                            <ThemedText style={styles.pokemonName} type="headline">{data.name}</ThemedText>
                             <ThemedText type="subtitle2">#{id?.toString().padStart(3,'0')}</ThemedText>
                         </Row>
                         <View style={{height: 144, zIndex: 10}}>
@@ -38,7 +49,47 @@ const PokemonDetailsScreen = () => {
                             />
                         </View>
                         <View style={styles.body}>
-
+                            <Row style={styles.buttonContainer}>
+                                {data?.types?.map(type => {
+                                    return (
+                                        <View key={type.type.name} style={[styles.buttonType, {backgroundColor: Colors.type[type.type.name]}]}>
+                                            <ThemedText type="subtitle3">
+                                                {type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)}
+                                            </ThemedText>
+                                        </View>
+                                    )
+                                })}
+                            </Row>
+                            <ThemedText color={basePokemonColor} type="headline">About</ThemedText>
+                            <Row style={{height: 48}}>
+                                <View style={{flex:1, alignItems: 'center', justifyContent: 'space-between'}}>
+                                    <Row style={styles.attribute}>
+                                        <Image style={{width: 16, height: 16}} source={require('@/assets/images/weight.png')}/>
+                                        <ThemedText color='grayDark' type="body3">{data.weight/10} kg</ThemedText>
+                                    </Row>
+                                    <ThemedText color='grayMedium' type="body3">Weight</ThemedText>
+                                </View>
+                                <View style={{flex:1, alignItems: 'center', justifyContent: 'space-between'}}>
+                                    <Row style={{gap: 8}}>
+                                        <Image style={{width: 16, height: 16}} source={require('@/assets/images/straighten.png')}/>
+                                        <ThemedText color='grayDark' type="body3">{data.height/10} m</ThemedText>
+                                    </Row>
+                                    <ThemedText color='grayMedium' type="body3">Height</ThemedText>
+                                </View>
+                                <View style={styles.ability}>
+                                    <Row style={{gap: 8}}>
+                                        <ThemedText style={{textAlign: 'center'}} color='grayDark' type="body3">{abilitiesDisplay}</ThemedText>
+                                    </Row>
+                                    <ThemedText color='grayMedium' type="body3">Moves</ThemedText>
+                                </View>
+                            </Row>
+                            <View style={{width: '100%'}}>
+                                <ThemedText color='grayDark' type="body3">{bio}</ThemedText>
+                            </View>
+                            <ThemedText color={basePokemonColor} type="headline">Base Stats</ThemedText>
+                            <View>
+                                <StatsView statList={statList} baseColor={basePokemonColor}/>
+                            </View>
                         </View>
                     </>
                     )
@@ -70,7 +121,28 @@ const styles = StyleSheet.create({
     body: {
         flex: 1,
         borderRadius: 4,
-        backgroundColor: 'white'
+        alignItems: 'center',
+        backgroundColor: 'white',
+        paddingTop: 50,
+        gap: 16,
+        paddingHorizontal: 20,
+    },
+    buttonContainer: {
+        gap: 16
+    },
+    buttonType: {
+        textTransform: 'capitalize',
+        borderRadius: 30,
+        paddingHorizontal: 12,
+        paddingVertical: 6
+    },
+    attribute: {
+        justifyContent: 'space-between'
+    },
+    ability: {
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'space-between',
     }
 })
 
